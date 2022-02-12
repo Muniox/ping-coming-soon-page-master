f91810c04866f47dec0f581a70c807e46720ffb8
Muniox <muniox20@gmail.com>
2022-02-12 14:10:07 +0100

    Finished JS validation for email input

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ const emailInput = document.querySelector(".site-preview__form-input");
 const regex =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-const el = document.querySelector(".site-preview__input-message");
+let check = false;
 
 function showError(value) {
   if (value === true) {
     return true;
-  } else if (value === false) {
+  } else if ((value === false) & (check === false)) {
     //wyswietlenie komunikatu
     const errorMessage = document.createElement("div");
     const newContent = document.createTextNode(
@@ -19,7 +19,8 @@ function showError(value) {
     emailInput.after(errorMessage);
     //wyswietlenie czerwonego bordera dla inputa
     emailInput.classList.add("site-preview__form-input--invalid");
-  }
+    check = true;
+  } else return null;
 }
 
 function isCorrectEmail(email, reg) {
@@ -33,9 +34,12 @@ function isCorrectEmail(email, reg) {
 }
 
 function resetMessage() {
-  const message = document.querySelector(".site-preview__input-message");
-  message.remove();
-  emailInput.classList.remove("site-preview__form-input--invalid");
+  if (check === true) {
+    const message = document.querySelector(".site-preview__input-message");
+    message.remove();
+    emailInput.classList.remove("site-preview__form-input--invalid");
+    check = false;
+  } else return null;
 }
 
 button.addEventListener("click", () =>
@@ -43,3 +47,4 @@ button.addEventListener("click", () =>
 );
 
 emailInput.addEventListener("click", () => resetMessage());
+emailInput.addEventListener("input", () => resetMessage());
